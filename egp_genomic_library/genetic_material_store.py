bf7c9fd3c1883ccd3f9db55fc1186e1c85a4a510
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2021-11-28 14:24:31 -0500

    Latest

---
+++ egp_genomic_library/genetic_material_store.py
@@ -0,0 +1,119 @@
+"""Genetic Material Store
+
+The GMS is a abstract base class for retrieving genetic codes.
+"""
+
+from random import randint
+from logging import DEBUG, NullHandler, getLogger
+from networkx import MultiDiGraph, union, spring_layout, relabel_nodes, get_node_attributes
+
+
+# Logging
+_logger = getLogger(__name__)
+_logger.addHandler(NullHandler())
+_LOG_DEBUG = _logger.isEnabledFor(DEBUG)
+
+
+# Constants
+_GC_COUNT = 'gc_count'
+_CODON_COUNT = 'num_codons'
+_OBJECT = 'object'
+_ZERO_GC_COUNT = {_GC_COUNT: 0, _CODON_COUNT: 0}
+
+
+class genetic_material_store():
+
+    def __init__(self, node_label='ref', left_edge_label='gca_ref', right_edge_label='gcb_ref'):
+        """Define the keys used to build the GC graph."""
+        self.nl = node_label
+        self.lel = left_edge_label
+        self.rel = right_edge_label
+        self.graph = MultiDiGraph()
+
+    def select(self):
+        raise NotImplementedError
+
+    def random_descendant(self, node_label):
+        """Select a random descendant from the node with node_label.
+
+        nl may be selected.
+        Every node in the tree has a weight equal to the number of incoming edges. i.e. if a node
+        appears N times in the flattened tree it will have a weight of N.
+
+        Args
+        ----
+        nl (type(gc[nl])): The label of the node at the root of the tree to select from.
+
+        Returns
+        -------
+        list(type(gc[nl])): The path to the selected node. The first element in the list will be nl.
+        """
+        # Fast access
+        nl = self.nl
+        lel = self.lel
+        rel = self.rel
+
+        # Initialise search
+        path = [node_label]
+        idx = randint(0, self.graph.nodes[node_label][_OBJECT][_GC_COUNT] - 1)
+        node = self.graph.nodes[node_label][_OBJECT]
+        left_count = 0 if node[lel] is None else self.graph.nodes[node[lel]][_OBJECT][_GC_COUNT]
+        pos = left_count
+
+        while pos != idx:
+            if pos > idx:
+                path.append(node[lel])
+                pos -= left_count
+                node = self.graph.nodes[node[lel]][_OBJECT]
+                left_count = 0 if node[lel] is None else self.graph.nodes[node[lel]][_OBJECT][_GC_COUNT]
+                pos += left_count
+            if pos < idx:
+                path.append(node[rel])
+                node = self.graph.nodes[node[rel]][_OBJECT]
+                left_count = 0 if node[lel] is None else self.graph.nodes[node[lel]][_OBJECT][_GC_COUNT]
+                pos += left_count + 1
+
+        return path
+
+    def add_nodes(self, gc_iter):
+        """Add a GC nodes to the GMS graph.
+
+        Adds all the GCs as nodes in gc_iter that do not already exist in the GMS graph.
+        Edges are created between each GC added and its GCA & GCB sub-GC's if they are not None.
+        Edges are directed from GC to sub-GC.
+
+        gc_count is calculated for all added GC's.
+
+        Args
+        ----
+        gc_iter (iter(gc)): Iterable of GC's to add. Must include self.nl, self.lel & self.rel keys.
+        """
+        # Fast access
+        nl = self.nl
+        lel = self.lel
+        rel = self.rel
+
+        # Add all node that do not already exist & the edges between them & existing nodes
+        gc_list = [gc for gc in gc_iter if not self.graph.has_node(gc[nl])]
+        self.graph.add_nodes_from(((gc[nl], {_OBJECT: gc}) for gc in gc_list))
+        self.graph.add_edges_from(((gc[nl], gc[lel]) for gc in gc_list if gc[lel] is not None))
+        self.graph.add_edges_from(((gc[nl], gc[rel]) for gc in gc_list if gc[rel] is not None))
+
+        # Calculate node GC count
+        # GC count is the number of nodes in the sub-tree including the root node.
+        for gc in gc_list:
+            work_stack = [gc]
+            while work_stack:
+                tgc = work_stack[-1]
+                tgc_lel = tgc[lel]
+                tgc_rel = tgc[rel]
+                left_node = self.graph.nodes[tgc[lel]][_OBJECT] if tgc_lel is not None else _ZERO_GC_COUNT
+                right_node = self.graph.nodes[tgc[rel]][_OBJECT] if tgc_rel is not None else _ZERO_GC_COUNT
+                if _GC_COUNT not in left_node:
+                    work_stack.append(left_node)
+                elif _GC_COUNT not in right_node:
+                    work_stack.append(right_node)
+                else:
+                    work_stack.pop()
+                    tgc[_GC_COUNT] = left_node[_GC_COUNT] + right_node[_GC_COUNT] + 1
+                    tgc[_CODON_COUNT] = 1 if tgc[_GC_COUNT] == 1 else left_node[_CODON_COUNT] + right_node[_CODON_COUNT]
---
+++ tests/test_genetic_material_store.py
@@ -0,0 +1,73 @@
+"""Test the Genetic Material Store (GMS)"""
+
+import pytest
+from random import choice
+from collections import Counter
+from statistics import stdev
+from os.path import dirname, join
+from json import load
+from logging import NullHandler, getLogger
+
+from egp_genomic_library import genetic_material_store, _CODON_COUNT, _GC_COUNT
+
+_logger = getLogger(__name__)
+_logger.addHandler(NullHandler())
+
+
+# Load test data
+with open(join(dirname(__file__), 'data/genetic_material_store_test_cases.json'), 'r') as fileptr:
+    test_data = load(fileptr)
+
+
+def test_instanciation():
+    """Straight up intanciation test."""
+    gms = genetic_material_store()
+    assert gms
+
+
+@pytest.mark.parametrize("test_case_idx", range(len(test_data)))
+def test_graph_construction(test_case_idx):
+    """Construct graphs and verify meta data is as expected."""
+    gms = genetic_material_store("id", "left", "right")
+    test_case = test_data[test_case_idx]
+    gms.add_nodes(test_case)
+    for node in test_case:
+        if node['expected_gc_count'] != node[_GC_COUNT]:
+            _logger.error(f'Unexpected gc_count in {node}')
+        if node['expected_codons'] != node[_CODON_COUNT]:
+            _logger.error(f'Unexpected gc_count in {node}')
+        assert node['expected_gc_count'] == node[_GC_COUNT]
+        assert node['expected_codons'] == node[_CODON_COUNT]
+
+
+def test_root_random_descendant():
+    """Select a random descendant from root node.
+
+    Validation is done by verifying the all paths between nodes exist.
+    1000 iterations gives a 1 in 10**23 chance of a node in a 20 node graph not being chosen.
+    """
+    gms = genetic_material_store("id", "left", "right")
+    gms.add_nodes(test_data[2])
+    all_paths = {'A', 'AB', 'ABC', 'ABCD', 'ABCDE', 'ABCG', 'ABCGF', 'ABCGH', 'ABI', 'AJ',
+        'AJK', 'AJKL', 'AJKLM', 'AJKLN', 'AJKP', 'AJKPQ', 'AJKPQR', 'AJKPQRS', 'AJKPT', 'AJO'}
+    paths = {''.join(gms.random_descendant(test_data[2][0]['id'])) for _ in range(1000)}
+    assert all_paths == paths
+
+
+def test_random_descendant():
+    """Select a random descendant.
+
+    Validation is done by verifying all paths between all nodes exist.
+    4000 iterations gives a 1 in 10**22 chance of a 1 path in 81 not being chosen
+    """
+    gms = genetic_material_store("id", "left", "right")
+    gms.add_nodes(test_data[2])
+    all_paths = {'A', 'AB', 'ABC', 'ABCD', 'ABCDE', 'ABCG', 'ABCGF', 'ABCGH', 'ABI', 'AJ', 'AJK',
+        'AJKL', 'AJKLM', 'AJKLN', 'AJKP', 'AJKPQ', 'AJKPQR', 'AJKPQRS', 'AJKPT', 'AJO', 'B', 'BC',
+        'BCD', 'BCDE', 'BCG', 'BCGF', 'BCGH', 'BI', 'C', 'CD', 'CDE', 'CG', 'CGF', 'CGH', 'D',
+        'DE', 'E', 'F', 'G', 'GF', 'GH', 'H', 'I', 'J', 'JK', 'JKL', 'JKLM', 'JKLN', 'JKP', 'JKPQ',
+        'JKPQR', 'JKPQRS', 'JKPT', 'JO', 'K', 'KL', 'KLM', 'KLN', 'KP', 'KPQ', 'KPQR', 'KPQRS',
+        'KPT', 'L', 'LM', 'LN', 'M', 'N', 'O', 'P', 'PQ', 'PQR', 'PQRS', 'PT', 'Q', 'QR', 'QRS',
+        'R', 'RS', 'S', 'T'}
+    paths = {''.join(gms.random_descendant(choice(test_data[2])['id'])) for _ in range(4000)}
+    assert all_paths == paths
