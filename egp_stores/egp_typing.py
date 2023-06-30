02dba4e80938f6928e95e765a5b47cea804a04e5
Shaped Sundew <16618209+Shapedsundew9@users.noreply.github.com>
2023-06-30 14:13:17 -0400

    Latest

--- egp_stores/egp_typing.py
+++ egp_stores/egp_typing.py
@@ -1,8 +1,10 @@
+""" EGP stores typing."""
 from typing import NotRequired, TypedDict
 from pypgtable.typing import TableConfigNorm, TableConfig
 
 
 class GenePoolConfig(TypedDict):
+    """Gene Pool Config structure prior to normalization."""
     gene_pool: NotRequired[TableConfig]
     meta_data: NotRequired[TableConfig]
     gp_metrics: NotRequired[TableConfig]
@@ -10,6 +12,7 @@ class GenePoolConfig(TypedDict):
 
 
 class GenePoolConfigNorm(TypedDict):
+    """Gene Pool Config structure post normalization."""
     gene_pool: TableConfigNorm
     meta_data: TableConfigNorm
     gp_metrics: TableConfigNorm
@@ -17,15 +20,18 @@ class GenePoolConfigNorm(TypedDict):
 
 
 class AncestryKeys(TypedDict):
+    """Definition of ancestry keys in the graph."""
     edge_keys: tuple[str, ...]
     missing_keys: tuple[str, ...]
     lost_keys: tuple[str, ...]
 
 
 class StructureKeys(TypedDict):
+    """Definition of structure keys in the graph"""
     edge_keys: tuple[str, ...]
 
 
 class GmsGraphViews(TypedDict):
+    """Definition of the views in the graph."""
     structure: StructureKeys
     ancestry: AncestryKeys
