eeade1b80df3a2564ebaf1efab8167fd59f3637b
Shaped Sundew <16618209+Shapedsundew9@users.noreply.github.com>
2024-02-11 18:22:13 -0500

    Latest

--- egp_stores/gene_pool_cache.py
+++ egp_stores/gene_pool_cache.py
@@ -49,12 +49,12 @@ Bit of an anti-pattern for python but in this case the savings are worth it.
 """
 from gc import collect
 from typing import Any, overload
-from logging import Logger, getLogger, NullHandler
-from numpy import empty, int64, argsort, bytes_, int32, full, zeros
+from logging import Logger, getLogger, NullHandler, DEBUG
+from numpy import empty, int64, argsort, bytes_, int32, full, iinfo
 from numpy.typing import NDArray
 
 from egp_types.store import static_store, dynamic_store, DDSL
-from egp_types._genetic_code import _genetic_code, PURGED_GENETIC_CODE, EMPTY_GENETIC_CODE, EMPTY_GC_IDX, PURGED_GC_IDX
+from egp_types._genetic_code import _genetic_code, PURGED_GENETIC_CODE, EMPTY_GENETIC_CODE
 from egp_types.graph import graph
 from .genomic_library import genomic_library
 
@@ -62,44 +62,14 @@ from .genomic_library import genomic_library
 # Logging
 _logger: Logger = getLogger(__name__)
 _logger.addHandler(NullHandler())
+_LOG_DEBUG: bool = _logger.isEnabledFor(DEBUG)
+_LOG_DEEP_DEBUG: bool = _logger.isEnabledFor(DEBUG - 1)
 
 
 # Constants
-GPC_DEFAULT_SIZE: int = 2**10
+GPC_DEFAULT_SIZE: int = 2**4
+INT64_MAX: int = iinfo(int64).max
 
-
-class gc_index():
-    """A genetic code index."""
-    indices: NDArray[int32]
-    genetic_codes: NDArray[Any]
-
-    def __delitem__(self, idx: int) -> None:
-        """Free the specified index by pointing to the EMPTY_GENETIC_CODE."""
-        cls = type(self)
-        cls.indices[idx] = 0
-
-    def __getitem__(self, idx: int) -> Any:
-        """Return the genetic code at the specified index."""
-        cls = type(self)
-        return cls.genetic_codes[cls.indices[idx]]
-
-    def __setitem__(self, idx: int, val: int32 | int) -> None:
-        """Set the genetic code at the specified index."""
-        cls = type(self)
-        cls.indices[idx] = val
-
-
-class gca(gc_index):
-    """Genetic Code A index."""
-
-class gcb(gc_index):
-    """Genetic Code B index."""
-
-class ancestor_a(gc_index):
-    """Ancestor A index."""
-
-class ancestor_b(gc_index):
-    """Ancestor B index."""
 
 class gpc_ds_common(static_store):
     """Gene Pool Cache dynamic store common to all genetic code types."""
@@ -108,14 +78,15 @@ class gpc_ds_common(static_store):
         """Initialize the storage."""
         super().__init__(*args, **kwargs)
         self.signature: NDArray[bytes_] = empty((self._size, 32), dtype=bytes_)
+        self.generation: NDArray[int64] = empty(self._size, dtype=int64)
 
 
 class ds_index_wrapper():
     """Wrapper for dynamic store index."""
 
-    _index_mapping: NDArray[int32]
-    _dynamic_store: dynamic_store
-    _genetic_codes: NDArray[Any]
+    index_mapping: NDArray[int32]
+    dstore: dynamic_store
+    genetic_codes: NDArray[Any]
 
     def __init__(self, member: str) -> None:
         """Initialize the wrapper."""
@@ -128,21 +99,21 @@ class ds_index_wrapper():
     def __getitem__(self, idx: int) -> Any:
         """Return the object at the specified index."""
         cls = type(self)
-        mapping_idx: int32 = cls._index_mapping[idx]
+        mapping_idx: int32 = cls.index_mapping[idx]
         if mapping_idx == -1:
             # If there is no mapping then the attribute is dynamically calculated
-            return getattr(cls._genetic_codes[idx], self.member)()
-        return cls._dynamic_store[self.member][mapping_idx]
+            return getattr(cls.genetic_codes[idx], self.member)()
+        return cls.dstore[self.member][mapping_idx]
 
     def __setitem__(self, idx: int, val: Any) -> None:
         """Set the object at the specified index."""
         cls = type(self)
-        mapping_idx: int32 | int = cls._index_mapping[idx]
+        mapping_idx: int32 | int = cls.index_mapping[idx]
         # If the mapping has not yet been created then create it.
         if mapping_idx == -1:
-            mapping_idx = cls._dynamic_store.next_index()
-            cls._index_mapping[idx] = mapping_idx
-        cls._dynamic_store[self.member][mapping_idx] = val
+            mapping_idx = cls.dstore.next_index()
+            cls.index_mapping[idx] = mapping_idx
+        cls.dstore[self.member][mapping_idx] = val
 
 
 class common_ds_index_wrapper(ds_index_wrapper):
@@ -161,23 +132,15 @@ class gene_pool_cache(static_store):
         super().__init__(size)
         # Static store members
         self.genetic_code: NDArray[Any] = empty(self._size, dtype=_genetic_code)
-        self.gca: gc_index = gca()
-        gca.indices = zeros(self._size, dtype=int32)
-        gca.genetic_codes = self.genetic_code
-        self.gcb: gc_index = gcb()
-        gcb.indices = zeros(self._size, dtype=int32)
-        gcb.genetic_codes = self.genetic_code
+        self.gca: NDArray[Any] = empty(self._size, dtype=_genetic_code)
+        self.gcb: NDArray[Any] = empty(self._size, dtype=_genetic_code)
         self.graph: NDArray[Any] = empty(self._size, dtype=graph)
-        self.ancestor_a: gc_index = ancestor_a()
-        ancestor_a.indices = zeros(self._size, dtype=int32)
-        ancestor_a.genetic_codes = self.genetic_code
-        self.ancestor_b: gc_index = ancestor_b()
-        ancestor_b.indices = zeros(self._size, dtype=int32)
-        ancestor_b.genetic_codes = self.genetic_code
+        self.ancestor_a: NDArray[Any] = empty(self._size, dtype=_genetic_code)
+        self.ancestor_b: NDArray[Any] = empty(self._size, dtype=_genetic_code)
 
         # Utility static store members
         # Access sequence of genetic codes. Used to determine which ones were least recently used.
-        self.access_sequence: NDArray[int64] = empty(self._size, dtype=int64)
+        self.access_sequence: NDArray[int64] = full(self._size, INT64_MAX, dtype=int64)
         # Common dynamic store indices. -1 means not in the common dynamic store.
         self.common_ds_idx: NDArray[int32] = full(self._size, int32(-1), dtype=int32)
 
@@ -185,14 +148,10 @@ class gene_pool_cache(static_store):
         self._common_ds = dynamic_store(gpc_ds_common, max((size.bit_length() - 4, DDSL)))
 
         # Set up dynamic store member index wrappers
-        common_ds_index_wrapper._index_mapping = self.common_ds_idx
-        common_ds_index_wrapper._dynamic_store = self._common_ds
-        common_ds_index_wrapper._genetic_codes = self.genetic_code
+        common_ds_index_wrapper.index_mapping = self.common_ds_idx
+        common_ds_index_wrapper.dstore = self._common_ds
+        common_ds_index_wrapper.genetic_codes = self.genetic_code
         self._common_ds_members: dict[str, common_ds_index_wrapper] = {m: common_ds_index_wrapper(m) for m in self._common_ds.members}
-
-        # Initialise the genetic code index
-        self.genetic_code[EMPTY_GC_IDX] = EMPTY_GENETIC_CODE
-        self.genetic_code[PURGED_GC_IDX] = PURGED_GENETIC_CODE
 
     @overload
     def __getitem__(self, item: int) -> _genetic_code: ...
@@ -210,18 +169,19 @@ class gene_pool_cache(static_store):
         if isinstance(item, int):
             return self.genetic_code[item]
         # First see if a member is in the static store
-        member: Any = getattr(gene_pool_cache, item, None)
+        member: Any = getattr(self, item, None)
         return member if member is not None else self._common_ds_members[item]
 
     def __delitem__(self, idx: int) -> None:
         """Free the specified index. Note this does not try and remove all references as purge() does."""
         #TODO: Push to genomic library
         self.genetic_code[idx] = PURGED_GENETIC_CODE
-        self.gca[idx] = 0
-        self.gcb[idx] = 0
+        self.gca[idx] = EMPTY_GENETIC_CODE
+        self.gcb[idx] = EMPTY_GENETIC_CODE
         self.graph[idx] = None
-        self.ancestor_a[idx] = 0
-        self.ancestor_b[idx] = 0
+        self.ancestor_a[idx] = EMPTY_GENETIC_CODE
+        self.ancestor_b[idx] = EMPTY_GENETIC_CODE
+        self.access_sequence[idx] = INT64_MAX
         super().__delitem__(idx)
         if self.common_ds_idx[idx] != -1:
             del self._common_ds[self.common_ds_idx[idx]]
@@ -233,14 +193,14 @@ class gene_pool_cache(static_store):
         self.genetic_code[idx] = obj
         return idx
 
-    def next_idx(self) -> int:
+    def next_index(self) -> int:
         """Return the next available index. If there are no more purge the genetic codes that have not been
         used in the longest time."""
         try:
-            idx: int = self.next_index()
+            idx: int = super().next_index()
         except OverflowError:
             self.purge()
-            idx = self.next_index()
+            idx = super().next_index()
         return idx
 
     def reset(self, size: int | None = None) -> None:
@@ -249,32 +209,24 @@ class gene_pool_cache(static_store):
         super().reset(size)
         # Static store members
         self.genetic_code: NDArray[Any] = empty(self._size, dtype=_genetic_code)
-        self.gca: gc_index = gca()
-        gca.indices = zeros(self._size, dtype=int32)
-        gca.genetic_codes = self.genetic_code
-        self.gcb: gc_index = gcb()
-        gcb.indices = zeros(self._size, dtype=int32)
-        gcb.genetic_codes = self.genetic_code
+        self.gca: NDArray[Any] = empty(self._size, dtype=_genetic_code)
+        self.gcb: NDArray[Any] = empty(self._size, dtype=_genetic_code)
         self.graph: NDArray[Any] = empty(self._size, dtype=graph)
-        self.ancestor_a: gc_index = ancestor_a()
-        ancestor_a.indices = zeros(self._size, dtype=int32)
-        ancestor_a.genetic_codes = self.genetic_code
-        self.ancestor_b: gc_index = ancestor_b()
-        ancestor_b.indices = zeros(self._size, dtype=int32)
-        ancestor_b.genetic_codes = self.genetic_code
+        self.ancestor_a: NDArray[Any] = empty(self._size, dtype=_genetic_code)
+        self.ancestor_b: NDArray[Any] = empty(self._size, dtype=_genetic_code)
 
         # Utility static store members
         # Access sequence of genetic codes. Used to determine which ones were least recently used.
-        self.access_sequence: NDArray[int64] = empty(self._size, dtype=int64)
+        self.access_sequence: NDArray[int64] = full(self._size, INT64_MAX, dtype=int64)
         # Common dynamic store indices. -1 means not in the common dynamic store.
         self.common_ds_idx: NDArray[int32] = full(self._size, int32(-1), dtype=int32)
 
-        # Initialise the genetic code index
-        self.genetic_code[EMPTY_GC_IDX] = EMPTY_GENETIC_CODE
-        self.genetic_code[PURGED_GC_IDX] = PURGED_GENETIC_CODE
+        # Re-initialize the common dynamic store wrapper
+        common_ds_index_wrapper.index_mapping = self.common_ds_idx
+        common_ds_index_wrapper.genetic_codes = self.genetic_code
 
         # Clean up the heap
-        _logger.debug(f"{collect()} unreachable objects not collected after purge.")
+        _logger.debug(f"{collect()} unreachable objects not collected after reset.")
 
         # Total = 2* 8 + 5 * 4 = 36 bytes + base class per element
 
@@ -286,7 +238,9 @@ class gene_pool_cache(static_store):
         num_to_purge: int = int(self._size * fraction)
         _logger.info(f"Purging {int(100*fraction)}% = ({num_to_purge} of {self._size}) of the store")
         purge_indices: set[int] = set(argsort(self.access_sequence)[:num_to_purge])
-
+        if _LOG_DEEP_DEBUG:
+            _logger.debug(f"Purging indices: {purge_indices}")
+            _logger.debug(f"Access sequence numbers {self.access_sequence}")
         # Convert GC's with purged dependents into leaf nodes
         for gc in self.genetic_code:
             gc.make_leaf(purge_indices)
@@ -295,8 +249,7 @@ class gene_pool_cache(static_store):
         for idx in purge_indices:
             del self[idx]
 
-
-        # Clean up the heap
+        # Clean up the heap: Intentionally calleding collect() regardless of the debug level.
         _logger.debug(f"{collect()} unreachable objects not collected after purge.")
 
 
